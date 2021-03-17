c43cef9da24ef08114e2402e83dbf6dfe4e93cf5
Alexandre Guméry <gumery.alexandre@gmail.com>
2021-03-17 17:20:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+a=float(input('Please instert a decimal number :'))
+a=int(a)
+outpoutBinary=bin(a)
+outpoutHexadecimal = hex(a)
+
+print("Binary of", a, "is", outpoutBinary)
+print("Hexadeciamel of", a, "is", outpoutHexadecimal)
